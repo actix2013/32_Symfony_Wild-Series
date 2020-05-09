534cffbfbdfcb6404cf387ad0195135f8c11a543
Guillaume CAVALIE <actix2013@gmail.com>
2020-05-09 16:03:49 +0200

    Custimize for chanllege 04 OK

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.scss in this case)
+import '../css/global.scss';
 import '../css/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
